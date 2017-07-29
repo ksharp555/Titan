9a1827430d5b1adb8422ddb55e55354b89bcd8a3
ksharp555 <jiahechang01@gmail.com>
2017-07-29 10:53:02 -0700

    recommendation function

--- src/rpc/ItemHistory.java
+++ src/rpc/ItemHistory.java
@@ -3,6 +3,7 @@ package rpc;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,6 +17,7 @@ import org.json.JSONObject;
 
 import db.DBConnection;
 import db.DBConnectionFactory;
+import entity.Item;
 
 /**
  * Servlet implementation class ItemHistory
@@ -38,7 +40,19 @@ public class ItemHistory extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String userId = request.getParameter("user_id");
+		   Set<Item> items = conn.getFavoriteItems(userId);
+		   JSONArray array = new JSONArray();
+		   for (Item item : items) {
+		     JSONObject obj = item.toJSONObject();
+		     try {
+		       obj.append("favorite", true);
+		     } catch (JSONException e) {
+		       e.printStackTrace();
+		     }
+		     array.put(obj);
+		   }
+		   RpcHelper.writeJsonArray(response, array);
 	}
 
 	/**
@@ -62,6 +76,25 @@ public class ItemHistory extends HttpServlet {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	   try {
+	     JSONObject input = RpcHelper.readJsonObject(request);
+	     String userId = input.getString("user_id");
+	     JSONArray array = (JSONArray) input.get("favorite");
+	
+	     List<String> histories = new ArrayList<>();
+	     for (int i = 0; i < array.length(); i++) {
+	       String itemId = (String) array.get(i);
+	       histories.add(itemId);
+	     }
+	     conn.unsetFavoriteItems(userId, histories);
+	     RpcHelper.writeJsonObject(response, new JSONObject().put("result", "SUCCESS"));
+	   } catch (JSONException e) {
+	     e.printStackTrace();
+	   }
 	}
 
 }
